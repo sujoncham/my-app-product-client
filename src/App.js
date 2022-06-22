3e434618c718c8aa3b96448e21eb1badf4ad7bb0
sujon-chambugong <sujonchambugong@gmail.com>
2022-06-22 13:23:52 +0600

    new React Doc

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div class="card card-compact w-96 bg-base-100 shadow-xl">
+  <figure><img src="https://api.lorem.space/image/shoes?w=400&h=225" alt="Shoes" /></figure>
+  <div class="card-body">
+    <h2 class="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+    </div>
+  );
+}
+
+export default App;
